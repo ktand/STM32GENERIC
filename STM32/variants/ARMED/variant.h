006921d2bd49a67dae5da00a4d3200fa048e5056
Karl Andersson <karl@iaccess.se>
2018-02-21 21:43:23 +0100

    Added board and variant definitions for ArmEd board

---
+++ STM32/variants/ARMED/variant.h
@@ -0,0 +1,18 @@
+#ifndef VARIANT_H
+#define VARIANT_H
+
+//*************** LED definitions *************************
+#define LED_BUILTIN PB7
+#define STM32_LED_BUILTIN_ACTIVE_LOW
+
+//*************** SPI pins ********************************
+#define MOSI PA7
+#define MISO PA6
+#define SCK PA5
+#define SS PE7
+
+//*************** I2C pins ********************************
+#define SDA PB9
+#define SCL PB8
+
+#endif
